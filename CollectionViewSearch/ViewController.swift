2f8d724cdc7dcdfdc20b16211d68dc5382973cdd
Jeremiah Gage <jeremiah@doubleencore.com>
2015-08-12 19:43:33 -0600

    Initial Commit

---
+++ CollectionViewSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionViewSearch
+//
+//  Created by Jeremiah Gage on 8/12/15.
+//  Copyright © 2015 Jeremiah Gage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
